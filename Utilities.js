f375a03d25bd9b327aeccd533da31db0bc75acfc
￼Jack tru￼ <165608541+Multiplication-golf@users.noreply.github.com>
2025-01-16 14:21:17 -0500

    Update Utilities.js

--- Utilities.js
+++ Utilities.js
@@ -18,6 +18,25 @@ export class Utilities () {
     while (angle < -Math.PI) angle += 2 * Math.PI;
     return angle;
   }
+
+  static mix(rgb, rgb2, percent, ...otherrgbs) {
+    let _return = false;
+    rgb.forEach((f) => {
+      if (typeof f !== "number") _return = true;
+    });
+    rgb2.forEach((f) => {
+      if (typeof f !== "number") _return = true;
+    });
+    if (rgb.length !== 3 || rgb2.length !== 3) _return = true;
+    if (_return) throw new Error("Bad rgbs");
+    var newrgb = rgb.map((c, i) => {
+      return (c = rgb2[i] * percent + c * (1 - percent));
+    });
+    otherrgbs.forEach((rgb, i) => {
+      mix(rgb, otherrgbs[i], percent);
+    });
+    return newrgb;
+  }
   
   static calculateTriangleVertices(cx, cy, size, angle) {
     const height = sqrt23 * size;
